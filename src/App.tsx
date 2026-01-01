3449c4ee4a28229cdb4d8a72e47cc90b64881de2
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-01-01 19:17:43 -0300

    atualizacion css

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
-
+import { TimerIcon } from 'lucide-react';
 
 export function App(){
 
     return (  
    
    <>
-      <Heading attr={123} attr2='string'>Hello world</Heading>
+      <Heading>
+        Hello world
+        
+        <button>
+          <TimerIcon/>
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex modi enim
         quae ipsam? Repellendus placeat sint enim voluptas dolorem similique ad
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,6 +1,10 @@
 import styles from './Heading.module.css';
 
+type HeadingProps = {
+    children: React.ReactNode;
 
-export function Heading(props){
-    return <h1 className={styles.heading}>{props.children}</h1>;
+}
+
+export function Heading({children}: HeadingProps){
+    return <h1 className={styles.heading}>{children}</h1>;
 }
