4352b255878cb6513fa2a799719cb72492c901f4
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-01-01 16:41:30 -0300

    atualizando app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { Heading } from './components/Heading';
 
-function App() {
-  const [count, setCount] = useState(0);
+import './styles/theme.css';
+import './styles/global.css';
 
-  return (
-    <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+
+export function App(){
+
+    return (  
+   
+   <>
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex modi enim
+        quae ipsam? Repellendus placeat sint enim voluptas dolorem similique ad
+        non! Culpa quidem facere recusandae, dignissimos ea iusto eligendi?
       </p>
-    </>
-  );
-}
+   </>
+   
+    );
+};
 
-export default App;
+
