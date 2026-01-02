feb955fb983fd089267338499992bad2013f559b
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-01-02 19:08:07 -0300

    add modifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
-import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
 
 export function App(){
 
-    return (  
-   
-   <>
-      <Heading>
-        Hello world
-        
-        <button>
-          <TimerIcon/>
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex modi enim
-        quae ipsam? Repellendus placeat sint enim voluptas dolorem similique ad
-        non! Culpa quidem facere recusandae, dignissimos ea iusto eligendi?
-      </p>
+    return<>
+
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
+      
    </>
-   
-    );
 };
 
 
