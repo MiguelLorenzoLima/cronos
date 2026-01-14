fb2b91f958541fc28fe5472fb49f4093c6934286
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-01-13 22:56:06 -0300

    padronizando pastas

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,22 @@
 
 import './styles/theme.css';
 import './styles/global.css';
-
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App(){
 
     return<>
 
-      <div className="container">
-        <div className="content">
-          <section>LOGO</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>  
 
-      <div className="container">
-        <div className="content">
-          <section>MENU</section>
-        </div>
-      </div>
 
-      <div className="container">
-        <div className="content">
-          <section>FORM</section>
-        </div>
-      </div>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
 
-      <div className="container">
-        <div className="content">
-          <section>FOOTER</section>
-        </div>
-      </div>
-      
    </>
 };
 
