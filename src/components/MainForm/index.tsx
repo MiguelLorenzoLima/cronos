cd0a7fd2f273221bd593c198b0103d832c3d9d92
MiguelLorenzoLima <miguel.lorenzo.lb@gmail.com>
2026-03-12 23:58:02 -0300

    next Cycle

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,13 +3,55 @@ import { DefaultButton } from "../DefaultButton";
 import { Cycles } from "../Cycles";
 import { DefaultInput } from "../DefaultInput";
 import { useRef } from "react";
+import type { TaskModel } from "../../models/TaskModel";
+import { useTaskContext } from "../../Contexts/TaskContext/useTaskContext";
+import { getNextCycle } from "../../utils/getNextCycle";
 
 
 export function MainForm(){
 
+  const {state, setState} = useTaskContext();
+
   const taskNameInput = useRef<HTMLInputElement>(null);
+  
+  const nextCycle = getNextCycle(state.currentCycle);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>){
     event.preventDefault();
+
+    if(taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+    console.log(taskName);
+
+    if (!taskName){
+      alert("Digite o nome da tarefa");
+       return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration*60
+
+    setState(prevState => {
+      return{
+        ...prevState,
+        config: {...prevState.config},
+        activeTask:newTask,
+        currentCycle: nextCycle,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      }
+    })
 
   }
 
